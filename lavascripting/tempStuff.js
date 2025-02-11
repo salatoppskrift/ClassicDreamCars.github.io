e12162568dfefc2293af92d141460a04b343deb3
Martin R <martin.ristvedt@gmail.com>
2025-02-12 00:52:49 +0100

    justerte så listene kan lage seg selv

--- lavascripting/tempStuff.js
+++ lavascripting/tempStuff.js
@@ -1,91 +1,74 @@
-const xxx = `
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-<br><br>
-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-<br><br>
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-`; // EXAMPLE TEXT. DELETE LATER.
-
 const dreamcarRules = [
   "Bilen må være i svært god stand",
   "Bilen må kun ha to dører",
   "Bilen må ha minst seks sylindre",
   "Bilen må være spesiell, det skal være en “dream car” – kjører man en times tur skal sjansen for å møte en tilsvarende bil være marginal"
 ];
+const YTreccs = ["Jay Leno's Garage*", "Doug Demuro*", "Seen Through Glass*", "Schmee150*", "Top Gear", "Car TV", "Car WOW", "Daily Driven Exotics", "Harry's Garage*", "Vehicle Virgins", "Petrolicious*", "Dreamcars daily", "Classic Cars", "Series No Cuts*", "Car News TV", "The Drive*", "Curves Magazin*", "Amazing Scenic Drives*", "Roadtrip Through the Lens*"];
+
+function usersLists(listparam){
+  let xx = "";
+  for(let i = 0; i < listparam.length; i++){
+    xx += `<li>${listparam[i]}</li>`;
+  }
+  return `<ul>${xx}</ul>`;
+}
 
 const txtOm = `
-Min interesse for bil og motor begynte tidlig. Da jeg var fem år kunne jeg navnet på de fleste bilmerkene. Den første bilen (en BMW 2002 tii, 1972 modell) ble kjøpt da jeg var 16 år og møysommelig satt i topp stand til den store dagen (18 år og førerkort). Siden har det blitt svært mange biler, rundt 40 totalt.
-<br><br>
-De siste årene har interessen for klassiske biler og sportsbiler vokst frem. Hva som kan sies å være en “klassisk” bil, har neppe noen fast definisjon. Det samme gjelder hva som kan sies å være en klassisk “drømmebil” (classic dream car). Med sportsbil tenker jeg på biler lagd for sportslig kjøring, dvs. kraftig motor og gode kjøreegenskaper. Fellesbetegnelsen blir “dream car” Jeg anser at følgende kriterier bør være på plass før betegnelsen “dream car” kan forsvares:
-<ul>
-  <li>
-    ${dreamcarRules[0]}
-  </li>
-  <li>
-    ${dreamcarRules[1]}
-  </li>
-  <li>
-    ${dreamcarRules[2]}
-  </li>
-  <li>
-    ${dreamcarRules[3]}
-  </li>
-</ul>
-Min nåværende “samling” av drømmebiler finnes under “biler” på denne nettsiden. Som det fremgår er den eldste bilen over 50 år og den nyeste er rundt fem år. Alle bilene har sin spesielle historie. Det er vanskelig å peke ut en favoritt. De er alle spesielle og kjære. Samtlige biler lagres varmt innendørs om vinteren. I løpet av vår-, sommer- og høstsesongen, kjøres de kun i pent vær og maksimalt et par tusen kilometer.
-<br><br>
-Som man vil skjønne er dette min lille private nettside. Jeg håper entusiaster kan ha litt glede av den og at nettsiden kan inspirere noen til å realisere sine bildrømmer. Livet er kort og bør leves til det fulle mens man kan. For noen er det likegyldig hvilken bil de kjører. Slik er det ikke for meg.
-<br><br><br>
-Enjoy the website! This is a site for my hobby.
+  Min interesse for bil og motor begynte tidlig. Da jeg var fem år kunne jeg navnet på de fleste bilmerkene. Den første bilen (en BMW 2002 tii, 1972 modell) ble kjøpt da jeg var 16 år og møysommelig satt i topp stand til den store dagen (18 år og førerkort). Siden har det blitt svært mange biler, rundt 40 totalt.
+  <br><br>
+  De siste årene har interessen for klassiske biler og sportsbiler vokst frem. Hva som kan sies å være en “klassisk” bil, har neppe noen fast definisjon. Det samme gjelder hva som kan sies å være en klassisk “drømmebil” (classic dream car). Med sportsbil tenker jeg på biler lagd for sportslig kjøring, dvs. kraftig motor og gode kjøreegenskaper. Fellesbetegnelsen blir “dream car” Jeg anser at følgende kriterier bør være på plass før betegnelsen “dream car” kan forsvares:
+  ${usersLists(dreamcarRules)}
+  Min nåværende “samling” av drømmebiler finnes under “biler” på denne nettsiden. Som det fremgår er den eldste bilen over 50 år og den nyeste er rundt fem år. Alle bilene har sin spesielle historie. Det er vanskelig å peke ut en favoritt. De er alle spesielle og kjære. Samtlige biler lagres varmt innendørs om vinteren. I løpet av vår-, sommer- og høstsesongen, kjøres de kun i pent vær og maksimalt et par tusen kilometer.
+  <br><br>
+  Som man vil skjønne er dette min lille private nettside. Jeg håper entusiaster kan ha litt glede av den og at nettsiden kan inspirere noen til å realisere sine bildrømmer. Livet er kort og bør leves til det fulle mens man kan. For noen er det likegyldig hvilken bil de kjører. Slik er det ikke for meg.
+  <div style="padding-top: 40px; padding-bottom: 40px;">
+  Enjoy the website! This is a site for my hobby.
+  </div>
 `;
 
-const YTreccs = ["Jay Leno's Garage*", "Doug Demuro*", "Seen Through Glass*", "Schmee150*", "Top Gear", "Car TV", "Car WOW", "Daily Driven Exotics", "Harry's Garage*", "Vehicle Virgins", "Petrolicious*", "Dreamcars daily", "Classic Cars", "Series No Cuts*", "Car News TV", "The Drive*", "Curves Magazin*", "Amazing Scenic Drives*", "Roadtrip Through the Lens*"];
-let xx = "";
-for(let i = 0; i < YTreccs.length; i++){
-  xx += `<li>${YTreccs[i]}</li>`;
-}
 //document.getElementById("ytreccsList").innerHTML = xx;
 
 const txtYT = `
-På YouTube er det mange fine «car channels». Du finner noen av disse ved å søke på ordene/navnene nedenfor. De som er merket med * er mine personlige favoritter:
-<br><br>
-<ul>
-  <li>${YTreccs[0]}</li>
-</ul>
+  På YouTube er det mange fine «car channels». Du finner noen av disse ved å søke på ordene/navnene nedenfor. De som er merket med * er mine personlige favoritter:
+  <br><br>
+  ${usersLists(YTreccs)}
 `;
 
 const txtKjrTurer = `
-Biler er lagd for å kjøres. Det gjelder også for klassiske biler.
-<br><br>
-Avhengig av hvor man bor i Norge, er "kjøresesongen" som regel april-oktober. Resten av året er det, grunnet vær og kjøreforhold (herunder salting av veiene), å anbefale at bilen lagres innendørs. Noen enkle lagringstips er å fylle opp bensintanken, pumpe ca. 25-30 % ekstra luft i dekkene, bruke en vedlikeholdslader, la vinduene stå noen centimeter åpne samt bruke et trekk over bilen. Om mulig er det fint om bilen under vinterlagringen beveges/rulles litt omtrent en gang i måneden (for å unngå at dekkene blir "flate").
-<br><br>
-Ved begynnelsen av kjøresesongen anbefales det å sjekke oljenivå, bremsevæske, radiator, batteri og dekker osv. Om mulig bør bilen i kjøresesongen få seg en luftetur på minst 20 minutter hver 14. dag. Småturer (under 10 minutter) bør unngås. Når man begynner å kjøre, anbefales det å kjøre ca. fem minutter rolig slik at motortemperaturen kommer opp til normalt nivå og oljen osv. kommer godt ut i "systemet" osv. Innimellom kan det være greit å "dra på litt" i løpet av turen for å få blåst ut motoren litt, men nødvendig er det som regel ikke.
-<br><br>
-I Norge er det mange fine kjøreturer. Det er bare å søke på nettet. Ofte opplever man de fineste turene om man tar mindre og litt svingete veier, med lite trafikk. Et annet tips, særlig hvis man skal på langtur, er de nasjonale turistveiene (som har en egen nettside). Personlig synes jeg det er flott å kjøre til Vestlandet med en klassisk bil og overnatte på ulike historiske hotell hvis mulig.
-<br><br>
-I Europa finnes det også mange flotte kjøreturer. Et tips er å fly til München, og leie en sportsbil der (for eksempel på Sixt bilutleie). Derfra er det forholdsvis kort vei til blant annet Østerrike og Sveits, i tillegg til at man får kjørt på Autobahn. Det er mange fine fjellpass som kan kjøres. De mest kjente er Grossglockner (Østerrike), Furka (Sveits) og Stelvio i Italia. Ønsker man arrangerte turer over for eksempel en ukes tid, finnes det firmaer som arrangerer dette (og ordner fine biler). Igjen er det bare å søke på nettet, og så vil lommeboken og øvrige preferanser avgjøre hva du velger.
-<br><br>
-Også i USA er det en rekke fine kjøreturer. Highway 1 i California er kjent for de fleste, men også Blue Ridge Parkway på østkysten kan anbefales.
-<br><br>
-<h2>"Flotte Kjøreturer"  |  "Sportslige Kjøreturer i Indre Vestfold"</h2>
-<br>
-<a href="https://9b2cf235-d98b-477e-9c59-089def9c4371.filesusr.com/ugd/1eab53_2b1c30cfe7104a2f89fca00d67a95ab4.pdf">link 1</a>
-<a href="https://9b2cf235-d98b-477e-9c59-089def9c4371.filesusr.com/ugd/1eab53_54b6566eaf104a42b4ab70a8e3ffdcd3.pdf">link 2</a>
-<br><br>
-Jeg har skrevet en bok som heter "Flotte Kjøreturer" som omtaler kjøreturer i Norge, Europa og USA. Videre har jeg laget et hefte som omtaler "Sportslige kjøreturer i indre Vestfold". Klikker du på pdf-versjoner vedlagt ovenfor, kan du lese om disse kjøreturene.
-<br><br><br>
-Lets Go!
+  Biler er lagd for å kjøres. Det gjelder også for klassiske biler.
+  <br><br>
+  Avhengig av hvor man bor i Norge, er "kjøresesongen" som regel april-oktober. Resten av året er det, grunnet vær og kjøreforhold (herunder salting av veiene), å anbefale at bilen lagres innendørs. Noen enkle lagringstips er å fylle opp bensintanken, pumpe ca. 25-30 % ekstra luft i dekkene, bruke en vedlikeholdslader, la vinduene stå noen centimeter åpne samt bruke et trekk over bilen. Om mulig er det fint om bilen under vinterlagringen beveges/rulles litt omtrent en gang i måneden (for å unngå at dekkene blir "flate").
+  <br><br>
+  Ved begynnelsen av kjøresesongen anbefales det å sjekke oljenivå, bremsevæske, radiator, batteri og dekker osv. Om mulig bør bilen i kjøresesongen få seg en luftetur på minst 20 minutter hver 14. dag. Småturer (under 10 minutter) bør unngås. Når man begynner å kjøre, anbefales det å kjøre ca. fem minutter rolig slik at motortemperaturen kommer opp til normalt nivå og oljen osv. kommer godt ut i "systemet" osv. Innimellom kan det være greit å "dra på litt" i løpet av turen for å få blåst ut motoren litt, men nødvendig er det som regel ikke.
+  <br><br>
+  I Norge er det mange fine kjøreturer. Det er bare å søke på nettet. Ofte opplever man de fineste turene om man tar mindre og litt svingete veier, med lite trafikk. Et annet tips, særlig hvis man skal på langtur, er de nasjonale turistveiene (som har en egen nettside). Personlig synes jeg det er flott å kjøre til Vestlandet med en klassisk bil og overnatte på ulike historiske hotell hvis mulig.
+  <br><br>
+  I Europa finnes det også mange flotte kjøreturer. Et tips er å fly til München, og leie en sportsbil der (for eksempel på Sixt bilutleie). Derfra er det forholdsvis kort vei til blant annet Østerrike og Sveits, i tillegg til at man får kjørt på Autobahn. Det er mange fine fjellpass som kan kjøres. De mest kjente er Grossglockner (Østerrike), Furka (Sveits) og Stelvio i Italia. Ønsker man arrangerte turer over for eksempel en ukes tid, finnes det firmaer som arrangerer dette (og ordner fine biler). Igjen er det bare å søke på nettet, og så vil lommeboken og øvrige preferanser avgjøre hva du velger.
+  <br><br>
+  Også i USA er det en rekke fine kjøreturer. Highway 1 i California er kjent for de fleste, men også Blue Ridge Parkway på østkysten kan anbefales.
+  <br><br>
+  <h2>"Flotte Kjøreturer"  |  "Sportslige Kjøreturer i Indre Vestfold"</h2>
+  <br>
+  <a href="https://9b2cf235-d98b-477e-9c59-089def9c4371.filesusr.com/ugd/1eab53_2b1c30cfe7104a2f89fca00d67a95ab4.pdf">link 1</a>
+  <a href="https://9b2cf235-d98b-477e-9c59-089def9c4371.filesusr.com/ugd/1eab53_54b6566eaf104a42b4ab70a8e3ffdcd3.pdf">link 2</a>
+  <br><br>
+  Jeg har skrevet en bok som heter "Flotte Kjøreturer" som omtaler kjøreturer i Norge, Europa og USA. Videre har jeg laget et hefte som omtaler "Sportslige kjøreturer i indre Vestfold". Klikker du på pdf-versjoner vedlagt ovenfor, kan du lese om disse kjøreturene.
+  <div style="padding-top: 40px; padding-bottom: 40px;">
+  Lets Go!
+  </div>
 `;
 
 const txtKnkt = `
-<div>
-  Per
-</div>
-<div>
-  9502119
-</div>
-<div>
-  Hvis du ønsker å sende meg en melding angående denne nettsiden, må du gjerne gjøre det på mobiltelefonnummeret angitt her.
-</div>
+  <div>
+    Per
+  </div>
+  <div>
+    9502119
+  </div>
+  <div>
+    Hvis du ønsker å sende meg en melding angående denne nettsiden, må du gjerne gjøre det på mobiltelefonnummeret angitt her.
+  </div>
 `;
 
 /*
